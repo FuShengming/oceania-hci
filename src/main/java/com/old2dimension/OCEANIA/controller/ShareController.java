30ba0ecc18e6dfd7c4e65ed9769b7c13f950b250
FuShengming <825820520@qq.com>
2020-12-23 13:03:27 +0800

    更改controller

--- src/main/java/com/old2dimension/OCEANIA/controller/ShareController.java
+++ src/main/java/com/old2dimension/OCEANIA/controller/ShareController.java
@@ -6,6 +6,7 @@ import com.old2dimension.OCEANIA.vo.UserAndCodeForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShareController {
     @Autowired
     ShareBL shareBL;
-    public ResponseVO receiveShareCode(@RequestBody UserAndCodeForm userAndCodeForm){
+
+    public void setShareBL(ShareBL shareBL) {
+        this.shareBL = shareBL;
+    }
+    @RequestMapping(value = "/acceptSharedProject",method = RequestMethod.POST)
+    public ResponseVO acceptSharedProject(@RequestBody UserAndCodeForm userAndCodeForm){
         return shareBL.acceptSharedProject(userAndCodeForm);
     }
 }
